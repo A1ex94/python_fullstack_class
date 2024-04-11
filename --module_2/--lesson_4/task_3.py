5d7a1ea4bfdc1d6788c15380002b33b6ef7ca06a
Alex <alexeymihailow@mail.ru>
2024-04-11 23:22:54 +0300

    lesson_4_task_3

---
+++ --module_2/--lesson_4/task_3.py
@@ -0,0 +1,4 @@
+dollar_price = float(input("Введите цену в долларах!"))
+exchange_rate = float(input("Введите курс рубля к доллару"))
+total_sum = float(dollar_price * exchange_rate)
+print(total_sum)
