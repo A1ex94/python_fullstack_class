adf63e95d2a6c2ac05080af2320bf4f6b2d48668
Alex <alexeymihailow@mail.ru>
2024-04-11 22:22:36 +0300

    lesson_2_re_3

--- --module_2/--lesson_2/task_3.py
+++ --module_2/--lesson_2/task_3.py
@@ -1,6 +1,6 @@
-books: int = (input("Введите кол-во книг"))
-stationery: float = (input("Введите кол-во концтоваров"))
-toys: int = (input("Введите кол-во игрушек"))
+books = int(input("Введите кол-во книг"))
+stationery = float(input("Введите кол-во концтоваров"))
+toys = int(input("Введите кол-во игрушек"))
 books *= 2
 stationery *= 1.5
 toys *= 3
