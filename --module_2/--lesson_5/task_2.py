346859ec61c225f38dbede90b9e6792376138e2e
Alex <alexeymihailow@mail.ru>
2024-04-21 19:24:11 +0300

    lesson_5

---
+++ --module_2/--lesson_5/task_2.py
@@ -0,0 +1,7 @@
+phrase_1 = "Welcome to the Creative Space"
+phrase_2 = "We don't have realive in here"
+phrase_3 = "Creative ideas for you"
+
+print(phrase_1.find("Creative"))
+print(phrase_2.find("Creative"))
+print(phrase_3.find("Creative"))
