97800560f2a047ea5d1f0db2e9bbf44c830febf3
Alex <alexeymihailow@mail.ru>
2024-04-21 19:07:05 +0300

    lesson_3_task_2_re2

--- --module_2/--lesson_3/task_2.py
+++ --module_2/--lesson_3/task_2.py
@@ -1,5 +1,5 @@
 sellers = ['Света', 'Олег', 'Маша', 'Паша']
 
-print('В чётные дни работают:', " ".join(coworkers[::2]))
+print('В чётные дни работают:', " ".join(sellers[::2]))
 print()
-print('В нечётные дни:', " ".join(coworkers[1::2]))
+print('В нечётные дни:', " ".join(sellers[1::2]))
